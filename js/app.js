c000274af5169e25ca6cfa04a2e6388f236c26ef
alejandro-dominguez <alejandro.dominguez1@live.com>
2023-04-14 13:51:18 -0300

    getting to export PDF

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
+const shortenText = (text, n) => {
+    if (text.length > n) {
+        const shortenedText = text.substring(0, n).concat("...")
+        return shortenedText
+    }
+    return text
+}
+
 $(document).ready(function() {
-    const getData = async (
-    ) => {
+    (async getFirestore => {
+        $("#spinnerContainer").addClass("d-flex")
+        $("body").css("overflow-y", "hidden")
         try {
             let carouselImgs = []
             let heroImg = []
@@ -44,7 +53,6 @@ $(document).ready(function() {
                     cardImgs.push(url)
                 })
             })
-            
             const getCardTexts = await db.collection("metroData").where("cardText", "==", true)
             .get()
             .then((querySnapshot) => {
@@ -54,11 +62,14 @@ $(document).ready(function() {
                 })
             })
             loadCards(cardImgs, cardTexts)
+            $("#spinnerContainer").addClass("d-none")
+            $("body").css("overflow-y", "auto")
         }
         catch (error) {
             console.error("Ha ocurrido un problema al cargar los datos del servidor", error)
-        }}
-    getData()
+        }
+        getStates()
+    })()
 })
 
 const loadCarousel = (carouselImgs) => {
@@ -96,7 +107,6 @@ const loadHeroSection = (heroImg, aboutUsText) => {
 const loadCards = (cardImgs, cardTexts) => {
     const cardsTemplate = document.querySelector("#servicesTemplate").content
     const cardsFragment = document.createDocumentFragment()
-    console.log(cardTexts)
     
     if (cardImgs.length && cardTexts.length > 0) {
         cardsTemplate.querySelector("#cardImg-1").setAttribute("src", cardImgs[0].url)
@@ -112,3 +122,286 @@ const loadCards = (cardImgs, cardTexts) => {
         console.error("Ha ocurrido un error al cargar los datos del servidor")
     }
 }
+
+const getStates = () => {
+    const settings = {
+        "url": "https://apis.datos.gob.ar/georef/api/provincias?campos=nombre",
+        "method": "GET",
+    }
+    
+    $.ajax(settings).done(function(response) {
+        $("#spinnerContainer").addClass("d-flex")
+        $("body").css("overflow-y", "hidden")
+        let stateNames = []
+        const statesArray = response.provincias
+        statesArray.forEach(state => {
+            stateNames.push(shortenText(state.nombre, 16))
+        })
+        stateNames.forEach(state => {
+            $("#ubication").append(`<option value="${state}">${state}</option>`)
+        })
+        $("#spinnerContainer").addClass("d-none")
+        $("body").css("overflow-y", "auto")
+    })
+    handlePricing()
+}
+
+const handlePricing = () => {
+    const pricingForm = $("#firstForm")
+
+    pricingForm.submit(function(e) {
+        e.preventDefault()
+        if ($("#meters").val() == 1 || $("#rooms").val() == 1 ) {
+            
+        } else {
+            
+        }
+        let ubication = $("#ubication").val()
+        let meters = $("#meters").val()
+        let rooms = $("#rooms").val()
+        let propertyType = $("#propertyType").val()
+        let ubicationValue
+        let roomsValue
+        let propertyTypeValue
+
+        switch (rooms) {
+            case "1":
+                roomsValue = "5"
+                break;
+            case "2":
+                roomsValue = "7"
+                break;
+            case "3":
+                roomsValue = "9"
+                break;
+            case "4":
+                roomsValue = "11"
+                break;
+            case "5":
+                roomsValue = "13"
+                break;
+            case "6":
+                roomsValue = "15"
+                break;
+            case "7":
+                roomsValue = "17"
+                break;
+            case "8":
+                roomsValue = "19"
+                break;
+            case "9":
+                roomsValue = "21"
+                break;
+            case "10":
+                roomsValue = "23"
+                break;
+        }
+
+        switch (propertyType) {
+            case "Departamento":
+                propertyTypeValue = "15"      
+                break;
+            case "Casa":
+                propertyTypeValue = "20"
+                break;
+        }
+
+        switch (ubication) {
+            case "Misiones":
+                ubicationValue = "900"
+                break;
+            case "San Luis":
+                ubicationValue = "1200"
+                break;
+            case "San Juan":
+                ubicationValue = "1500"
+                break;
+            case "Entre Ríos":
+                ubicationValue = "1500"
+                break;
+            case "Santa Cruz":
+                ubicationValue = "1200"
+                break;
+            case "Río Negro":
+                ubicationValue = "2000"
+                break;
+            case "Chubut":
+                ubicationValue = "1700"
+                break;
+            case "Córdoba":
+                ubicationValue = "1200"
+                break;
+            case "Mendoza":
+                ubicationValue = "1300"
+                break;
+            case "Chubut":
+                ubicationValue = "1500"
+                break;
+            case "La Rioja":
+                ubicationValue = "1100"
+                break;
+            case "Catamarca":
+                ubicationValue = "1000"
+                break;
+            case "La Pampa":
+                ubicationValue = "1200"
+                break;
+            case "Santiago del Est...":
+                ubicationValue = "1000"
+                break;
+            case "Corrientes":
+                ubicationValue = "1100"
+                break;
+            case "Santa Fe":
+                ubicationValue = "1500"
+                break;
+            case "Tucumán":
+                ubicationValue = "1100"
+                break;
+            case "Neuquén":
+                ubicationValue = "2500"
+                break;
+            case "Salta":
+                ubicationValue = "1200"
+                break;
+            case "Chaco":
+                ubicationValue = "900"
+                break;
+            case "Formosa":
+                ubicationValue = "800"
+                break;
+            case "Jujuy":
+                ubicationValue = "1000"
+                break;
+            case "Ciudad Autónoma ...":
+                ubicationValue = "2800"
+                break;
+            case "Buenos Aires":
+                ubicationValue = "2800"
+                break;
+            case "Tierra del Fuego...":
+                ubicationValue = "2500"
+                break;
+            default:
+                ubicationValue = "2000"
+                break;
+        }
+
+        let baseMetersValue = parseInt(Number(meters) * Number(ubicationValue))
+        let roomsPercentage = baseMetersValue * (roomsValue / 100)
+        let propertyTypePercentage = baseMetersValue * (propertyTypeValue / 100)
+        let totalPricing = baseMetersValue + roomsPercentage + propertyTypePercentage
+
+        $("#totalPricing").val(`USD$ ${totalPricing}`)
+        handlePricingSubmit()
+    })
+}
+
+const handlePricingSubmit = () => {
+    let view1 = $('#firstForm')
+    let view2 = $('#secondForm')
+    const pricingForms = $('.pricingForm')
+
+    Array.from(pricingForms).forEach(form => {
+        form.addEventListener('submit', e => {
+            if (form.checkValidity() == false) {
+                e.stopPropagation()
+                e.preventDefault()
+                form.classList.add('was-validated')
+            } else {
+                form.classList.add('was-validated')
+                e.stopPropagation()
+                e.preventDefault()
+                switch (form.id) {
+                    case "firstForm":
+                        view1.addClass('d-none')
+                        view2.removeClass('d-none')
+                        break;
+                    case "secondForm":
+                        showConfirmData();
+                        break;
+                }
+            }
+        }, false)
+    })
+}
+
+const showConfirmData = () => {
+    class PricingForm {
+        ubication;
+        meters;
+        rooms;
+        propertyType;
+        totalPricing;
+        fullName;
+        email;
+    }
+    let pricingForm = new PricingForm()
+
+    pricingForm.ubication = $("#ubication").val()
+    pricingForm.meters = $("#meters").val()
+    pricingForm.rooms = $("#rooms").val()
+    pricingForm.propertyType = $('#propertyType').val()
+    pricingForm.totalPricing = $('#totalPricing').val()
+    pricingForm.fullName = $('#fullName').val()
+    pricingForm.email = $("#email").val()
+    console.log(pricingForm.ubication, pricingForm.meters, pricingForm.rooms, pricingForm.propertyType, pricingForm.totalPricing,
+    pricingForm.fullName, pricingForm.email)
+    $('#ubicationConfirm').val(pricingForm.ubication)
+    $('#metersConfirm').val(pricingForm.meters)
+    $('#roomsConfirm').val(pricingForm.rooms)
+    $('#propertyTypeConfirm').val(pricingForm.propertyType)
+    $('#totalPricingConfirm').val(pricingForm.totalPricing)
+    $('#fullNameConfirm').val(pricingForm.fullName);
+    $('#emailConfirm').val(pricingForm.email);
+    $('#secondForm').addClass('d-none'); //ocultamos la vista 2
+    $('#thirdForm').removeClass('d-none'); //mostramos la vista de confirmación
+}
+
+/*
+const handlePricingSubmit = () => {   
+}
+    (() => {
+        const pricingForm = $("#firstForm")
+        const userForm = $("#secondForm")
+        const confirmForm = $("#thirdForm")
+
+        const forms = $(".needs-validation")
+        Array.from(forms).forEach(form => {
+            form.addEventListener("submit", e => {
+                if (form.checkValidity() == false) {
+                    e.stopPropagation()
+                    e.preventDefault()
+                } else {
+                    e.stopPropagation()
+                    e.preventDefault()
+                    
+                    switch(forms.id) {
+                        case "firstForm":
+                        pricingForm.addClass("d-none")
+                        userForm.removeClass("d-none")  
+                        break;
+                        case "secondForm":
+                        fillThirdForm()
+                        break;
+                        case "ThirdForm":
+                        sendEmail()
+                        break;
+                    }
+                }
+            })
+        })
+    })
+}
+ */
+
+
+
+
+
+/* class ContactForm {
+    fullName;
+    email;
+    message;
+}
+let contactForm = new ContactForm() */
